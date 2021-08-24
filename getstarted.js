9404ca75f9335b118b2f342349b31a5c62539866
jakuskii <74100491+jakuskii@users.noreply.github.com>
2021-08-24 17:15:51 +0200

    Add files via upload

---
+++ getstarted.js
@@ -0,0 +1,38 @@
+
+document.getElementById('60button').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal60').style.display = 'flex';
+});
+document.querySelector('.close60').addEventListener('click',
+function() {
+ document.querySelector('.bg-modal60').style.display = 'none';
+});
+
+
+document.getElementById('75button').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal75').style.display = 'flex';
+});
+document.querySelector('.close75').addEventListener('click',
+function() {
+ document.querySelector('.bg-modal75').style.display = 'none';
+});
+
+
+document.getElementById('80button').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal80').style.display = 'flex';
+});
+document.querySelector('.close80').addEventListener('click',
+function() {
+ document.querySelector('.bg-modal80').style.display = 'none';
+});
+
+document.getElementById('100button').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal100').style.display = 'flex';
+});
+document.querySelector('.close100').addEventListener('click',
+function() {
+ document.querySelector('.bg-modal100').style.display = 'none';
+});
